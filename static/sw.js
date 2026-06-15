73ff9ff72147d3369ab2f3c23e8520d18bddbbb6
DarkSword <mynetpar@gmail.com>
2026-06-15 16:05:23 +0300

    feat: Add PWA support (manifest, service worker, icons)

---
+++ static/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'getfoss-cache-v1';
+
+const PRECACHE_URLS = [
+  '/',
+  '/css/custom.css',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install Service Worker and precache base files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+// Clean up old cache on Service Worker update
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Strategy: Network First for HTML, Cache First for static assets
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/'))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        return cachedResponse || fetch(event.request).then(response => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  }
+});
